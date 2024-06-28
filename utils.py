f2fabc406ee811743eaecc9974a329b0d99d0204
johnsaveus <ioannis.savvas00@gmail.com>
2024-06-28 19:20:46 +0300

    Send_to_colab

--- utils.py
+++ utils.py
@@ -3,6 +3,13 @@ import os
 import pandas as pd
 
 def proccess_raw(raw_dir = 'Data/raw'):
+    """
+    Args: 
+        - raw_dir: Directory of unproccessed data that has genre folder containing songs in wav files
+    Returns:
+        - song_paths: Return the path of each song
+        - labels: Return the label of each song
+    """
     song_paths = []
     labels = []
     for root, _, files in os.walk(raw_dir):
@@ -14,7 +21,14 @@ def proccess_raw(raw_dir = 'Data/raw'):
                 labels.append(label)
     return song_paths, labels
 
-def stratified_split(song_paths, labels, train_ratio = 0.8, val_ratio = 0.1, save_dir = 'Data/splits'):
+def stratified_split(train_ratio = 0.8, val_ratio = 0.1, save_dir = 'Data/splits'):
+    """
+    Args:
+        - train_ratio
+        - val_ratio
+        - save_dir: Directory to save split csv_files with song_path and label
+    """
+    song_paths, labels = proccess_raw()
     test_ratio = 1 - train_ratio - val_ratio
     songs_train, songs_val_test, y_train , y_val_test = train_test_split(song_paths,
                                                                      labels,
@@ -29,20 +43,16 @@ def stratified_split(song_paths, labels, train_ratio = 0.8, val_ratio = 0.1, sav
     train_data = pd.DataFrame({'Path': songs_train, 'Label': y_train})
     val_data = pd.DataFrame({'Path': songs_val, 'Label': y_val})
     test_data = pd.DataFrame({'Path': songs_test, 'Label': y_test})
-
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-
     train_path = os.path.join(save_dir, 'train.csv')
     val_path = os.path.join(save_dir, 'val.csv')
     test_path = os.path.join(save_dir, 'test.csv')
-
-    # Save the DataFrames as CSV files
     train_data.to_csv(train_path, index=False)
     val_data.to_csv(val_path, index=False)
     test_data.to_csv(test_path, index=False)
-### Create Splits
 
-song_paths, labels = proccess_raw()
-stratified_split(song_paths = song_paths, labels = labels)
+if __name__ == "__main__":
+    # Create the csv of the splitted files
+    stratified_split()
 
